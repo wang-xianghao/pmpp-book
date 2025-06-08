68ffdabbb127da1ae82de3a0e778847dadbea144
xianghao <wang.xianghao@outlook.com>
2025-06-09 02:05:01 +1000

    change vector add directory

--- chapter2/vectAdd.cu
+++ chapter2/vectAdd.cu
@@ -6,7 +6,7 @@
 static int threadsPerBlock;
 static int r;
 
-__global__ void vectAddKernel(const float *A, const float *B, float *C, int N) {
+void vectAddKernel(const float *A, const float *B, float *C, int N) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < N) {
         C[idx] = A[idx] + B[idx];
